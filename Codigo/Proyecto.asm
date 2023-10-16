e3d9fca24395b5ea60d9bc755058cb15feed9fb7
Kycoon <jomaval4@gmail.com>
2023-10-15 23:56:32 -0600

    Boton de pausa y menu

--- Codigo/Proyecto.asm
+++ Codigo/Proyecto.asm
@@ -1,11 +1,11 @@
 .model small
 .stack 100h
-
 .data
+
 mensaje db "DVD",10,13,"$"
 col db 35
 row db 12
-
+menuOpciones db "1. Disenar escenario de juego", 10, 13, "2. Establecer nivel y nombre del jugador", 10, 13, "3. Establecer escenario", 10, 13, "4. Iniciar juego", 10, 13, "5. Acerca de", 10, 13, "6. Salir", 10, 13, "$"
 posicion macro x, y
     mov ah, 02H
     mov bh, 00h
@@ -18,21 +18,30 @@ endm
 .code
 mov ax,@DATA
 mov ds, ax
+menu:
+    call limpia
+    posicion 10, 10
+    
+	mov ah, 09h
+    lea dx, menuOpciones
+    int 21h
 
-ciclo:
     mov ah, 01h
     int 16h
-    jz noPause
+    jz menu
 
     mov ah, 00h
     int 16h
 
-    cmp AL, "P"
-    je waitForP
+    cmp al, '1'
+    je ciclo
+    cmp al, '6'
+    je salir
+ciclo:
 
-noPause:
+    posicion 10, 10
     call limpia
-    posicion 12,col
+    posicion 10,10
 
     mov ah,09h
     lea dx,mensaje
@@ -45,7 +54,10 @@ noPause:
     jne notA
     dec row
     notA:
-
+	
+    cmp AL, "p"
+    je waitForP
+	
     cmp AL,"s"
     jne notB
     inc row
@@ -68,12 +80,13 @@ waitForP:
     int 16h
     jz waitForP
     
-    mov ah, 00h
+    mov ah,010h
     int 16h
-    cmp AL, "P"
+    cmp AL, "p"
     jne waitForP
     jmp ciclo
 
+salir:
 mov ah, 04ch
 int 21h
 
